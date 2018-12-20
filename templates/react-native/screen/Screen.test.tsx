0bee73ce0cd68bdfea64d157f60b6f621c3220e0
hyochan <dooboolab@gmail.com>
2018-12-20 12:17:05 +0900

    Update templates.
    appSotre to be optionally used.

--- templates/react-native/screen/Screen.test.tsx
+++ templates/react-native/screen/Screen.test.tsx
@@ -1,7 +1,7 @@
 import 'react-native';
 import * as React from 'react';
 import Screen from '../Screen';
-import appStore from '../../../stores/appStore';
+// import appStore from '../../../stores/appStore';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
@@ -9,7 +9,8 @@ import { shallow, render } from 'enzyme';
 
 describe('rendering test', () => {
   const wrapper = shallow(
-    <Screen store={appStore}/>,
+    <Screen/>,
+    // <Screen store={appStore}/>,
   );
 
   it('renders as expected', () => {
