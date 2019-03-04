3fed9209797f854f8e04d145b96978fe047a1617
hyochan <dooboolab@gmail.com>
2019-03-04 17:20:55 +0900

    Removed model and store from template

--- templates/common/Model.js
+++
@@ -1,7 +0,0 @@
-import { observable } from 'mobx';
-
-class Model {
-  @observable value: number;
-}
-
-export default Model;
--- templates/common/Store.js
+++
@@ -1,8 +0,0 @@
-import { observable } from 'mobx';
-
-class ObservableListStore {
-  @observable value: number;
-}
-
-const observableListStore = new ObservableListStore();
-export default observableListStore;
