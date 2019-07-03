05de964ca488f4ba140fb9e47d799f385d57670b
hyochan <dooboolab@gmail.com>
2019-07-03 18:38:39 +0900

    Fixe more templates

--- templates/react/shared/Shared.js
+++ templates/react/shared/Shared.js
@@ -2,14 +2,10 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-import {
-  IC_MASK,
-} from '../../utils/Icons';
-
 const Container = styled.div`
-  display: flex,
-  width: '100px',
-  height: '40px',
+  display: flex;
+  width: '100px';
+  height: '40px';
 `;
 
 type Props = {
