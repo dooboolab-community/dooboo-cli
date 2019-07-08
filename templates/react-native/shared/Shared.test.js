bd52dcc0b80bddae7dfc9bff3d737ff9f9b2e9c3
hyochan <dooboolab@gmail.com>
2019-07-08 16:59:01 +0900

    Edit [SharedComponent] template

--- templates/react-native/shared/Shared.test.js
+++ templates/react-native/shared/Shared.test.js
@@ -4,11 +4,35 @@ import Shared from '../Shared';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import { fireEvent, render, RenderAPI } from 'react-native-testing-library';
 
-describe('[Shared]', () => {
+let props: object;
+let component: React.ReactElement;
+let testingLib: any;
+
+describe('[Shared] render', () => {
+  beforeEach(() => {
+    props = {
+    };
+    component = (
+      <Shared {...props} />
+    );
+  });
+
   it('renders without crashing', () => {
-    const rendered = renderer.create(<Shared />).toJSON();
+    const rendered = renderer.create(component).toJSON();
     expect(rendered).toMatchSnapshot();
     expect(rendered).toBeTruthy();
+  });
+
+  describe('interactions', () => {
+    beforeEach(() => {
+      testingLib = render(component);
+    });
+
+    it('should simulate onClick', () => {
+      // fireEvent(testingLib.getByTestId('btn'), 'click');
+      // expect(cnt).toBe(2);
+    });
   });
 });
--- templates/react/shared/Shared.test.js
+++ templates/react/shared/Shared.test.js
@@ -4,16 +4,36 @@ import renderer, { ReactTestRendererJSON } from 'react-test-renderer';
 import Shared from '../Shared';
 import { render, fireEvent, getByTestId } from '@testing-library/react';
 
-const props = {
-  history: {
-    goBack: jest.fn(),
-  },
-};
+let props: object;
+let component: React.ReactElement;
+let testingLib: any;
 
 describe('[Shared] render', () => {
+  beforeEach(() => {
+    props = {
+      history: {
+        goBack: jest.fn(),
+      },
+    };
+    component = (
+      <Shared />
+    );
+  });
+
   it('renders without crashing', () => {
-    const rendered = renderer.create(<Shared />).toJSON();
+    const rendered: ReactTestRendererJSON = renderer.create(component).toJSON();
     expect(rendered).toMatchSnapshot();
     expect(rendered).toBeTruthy();
+  });
+
+  describe('interactions', () => {
+    beforeEach(() => {
+      testingLib = render(component);
+    });
+
+    it('should simulate onClick', () => {
+      // fireEvent(testingLib.getByTestId('btn'), 'click');
+      // expect(cnt).toBe(2);
+    });
   });
 });
