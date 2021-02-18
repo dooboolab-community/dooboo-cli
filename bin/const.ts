9c50ae2bcfa6e30db7ce6f540a879b01253855f1
hyochan <dooboolab@gmail.com>
2021-02-18 14:19:18 +0900

    Release 6.1.1 with atomic design removed

--- bin/const.ts
+++ bin/const.ts
@@ -1,5 +1,5 @@
-export const RN_PROJECT_BRANCH = 'release/0.63.0-atomic-design';
+export const RN_PROJECT_BRANCH = 'release/0.63.0-atomic-design-removed';
 export const RN_VERSION = '0.63.0';
 
-export const EXPO_PROJECT_BRANCH = 'release/40-atomic-design';
+export const EXPO_PROJECT_BRANCH = 'release/40-atomic-design-removed';
 export const EXPO_VERSION = '40';
