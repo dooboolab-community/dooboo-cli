924ca5b4421a961d15ead103e44e2d253f1ad64d
hyochan <dooboolab@gmail.com>
2021-06-19 21:10:38 +0900

    Relesae 6.4.0
    Integrate [dooboo-ui](https://github.com/dooboolab/dooboo-ui) to `react-native` and `expo` projects.

--- bin/const.ts
+++ bin/const.ts
@@ -1,4 +1,4 @@
-export const RN_PROJECT_BRANCH = 'release/0.64.0-style-emotion';
+export const RN_PROJECT_BRANCH = 'release/0.64.0-dooboo-ui';
 export const RN_VERSION = '0.64.0';
 
-export const EXPO_PROJECT_BRANCH = 'release/41';
+export const EXPO_PROJECT_BRANCH = 'release/41-dooboo-ui';
