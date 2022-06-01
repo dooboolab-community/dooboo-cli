ab9f3dfe6329753a07a590b90479ab7f4ae783c2
hyochan <dooboolab@gmail.com>
2022-06-01 22:40:22 +0900

    Upgrade RN proj to 0.68+

--- bin/const.ts
+++ bin/const.ts
@@ -1,4 +1,4 @@
-export const RN_PROJECT_BRANCH = 'release/0.67.2';
-export const RN_VERSION = '0.67.4';
+export const RN_PROJECT_BRANCH = 'release/0.68+';
+export const RN_VERSION = '0.68.2';
 
 export const EXPO_PROJECT_BRANCH = 'release/45';
