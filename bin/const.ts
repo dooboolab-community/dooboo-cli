511be7c40b3e2cb4342e2e3bc01ec3d566e16ecd
hyochan <dooboolab@gmail.com>
2022-08-03 12:05:28 +0900

    Update expo template to SDK 46

--- bin/const.ts
+++ bin/const.ts
@@ -1,4 +1,4 @@
 export const RN_PROJECT_BRANCH = 'release/0.68+';
 export const RN_VERSION = '0.68.2';
 
-export const EXPO_PROJECT_BRANCH = 'release/45';
+export const EXPO_PROJECT_BRANCH = 'release/46';
