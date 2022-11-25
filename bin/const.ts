c3d78fa22428d7f5e4bcc5b0e782ad00fa40c252
hyochan <dooboolab@gmail.com>
2022-11-25 23:07:46 +0900

    8.5.0
    Support expo SDK 47

--- bin/const.ts
+++ bin/const.ts
@@ -1,4 +1,4 @@
 export const RN_PROJECT_BRANCH = 'release/0.69+';
 export const RN_VERSION = '0.69.3';
 
-export const EXPO_PROJECT_BRANCH = 'release/46';
+export const EXPO_PROJECT_BRANCH = 'release/47';
