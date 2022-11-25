8f3f061ac98ec36146a446ab5955793fc4ce676f
hyochan <dooboolab@gmail.com>
2022-11-25 23:39:47 +0900

    8.5.1
    Update RN version to 0.70.6

--- bin/const.ts
+++ bin/const.ts
@@ -1,4 +1,4 @@
-export const RN_PROJECT_BRANCH = 'release/0.69+';
-export const RN_VERSION = '0.69.3';
+export const RN_PROJECT_BRANCH = 'release/0.70+';
+export const RN_VERSION = '0.70.6';
 
 export const EXPO_PROJECT_BRANCH = 'release/47';
